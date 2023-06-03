363e8e04cd3aca78c66927a9a27e28b8aa717c74
msa <msai.sadehi@gmail.com>
2023-06-03 20:00:53 +0330

    first

---
+++ session1.py
@@ -0,0 +1,20 @@
+# print("ali" and "sara")
+
+# string = ""
+# string = ''
+# string = 'aaaa'
+# string[0] = 'b'
+
+# numbers = (1,)
+# print(type(numbers))
+
+# numbers = (1,2,3,4,)
+
+# numbers[0] = 11
+
+# numbers = {1,2,3,4}
+# print(type(numbers))
+# national_code = str(12344444)
+
+# print(national_code[0])
+
