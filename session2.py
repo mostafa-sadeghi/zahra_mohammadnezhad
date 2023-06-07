a756cbe9a2497f5a4390b2f60a828e172c1d0c64
msa <msai.sadehi@gmail.com>
2023-06-07 19:58:03 +0330

    session2

---
+++ session2.py
@@ -0,0 +1,115 @@
+# name = input('Enter your name:> ')
+# age = int(input('Enter your age:> '))
+
+# if age < 8:
+#     # print("%s your age is %s and you are kid"%(name,age))
+#     print(f'{name}, your age is {age} and you are kid')
+# # elif 8 <= age < 13:
+# elif age >= 8 and age <13:
+#     print(f'{name}, your age is {age} and you are junior')
+# elif 13 <= age < 18:
+#     print(f'{name}, your age is {age} and you are teenager')
+
+# else:
+#     print(f'{name}, your age is {age} and you are adult')
+
+
+# score1 = 78
+# score2 = 65
+# score3 = 86
+
+# average = (score1 + score2 + score3)/3
+# print(f'{average:.2f}')
+# if average >= 90:
+#     print("A")
+# elif average >= 80:
+#     print("B")
+# elif average >= 70:
+#     print("C")
+# elif average >= 60:
+#     print("D")
+# elif average >= 50:
+#     print("F")
+# else:
+#     print("G")
+
+
+# age = 16
+
+# if True:
+#     print("balalalal")
+#     if age >= 13:
+#         print("teenager")
+
+# if age >= 13 and age < 18:
+#     print("teenager")
+
+# if 13 <= age < 18:
+#     print("teenager")
+
+
+# a = 13
+# if a == 12:
+#     print(f'{a} = 12')
+#     print("ok")
+
+# print("outside")
+
+numbers = [1, 2, 3, 4, 5]
+# for n in numbers:
+#     print(n, end=" ")
+
+# for i in range(len(numbers)):
+#     print(numbers[i], end=" ")
+
+# for _ in range(10):  #
+#     print("hello")
+
+# for char in 'python':
+#     print(char.upper(), end=" ")
+
+# for number in [1,2,3,4,5]:
+#     print(number ** 2)
+
+# for number in (1,2,3,4,5):
+#     print(number ** 2)
+# for number in {1,2,3,4,5}:
+#     print(number ** 2)
+
+
+# my_dict = {
+#     1: "one",
+#     2: "two"
+# }
+# print(my_dict[1])
+
+# my_dict = {
+#     '1': "one",
+#     '2': "two"
+# }
+# print(my_dict['1'])
+
+# my_dict = {
+#     (1,2): "one",
+#     (3,4): "two"
+# }
+# print(my_dict[(3,4)])
+
+person_1 = {
+    'id': 1,
+    'name': "sara",
+    'age': 30
+}
+
+person_2 = {
+    'name': "sara",
+    'id': 1,
+    'age': 30
+}
+
+print(person_1 == person_2)
+
+# numbers_1 = [1,2,3,4]
+# numbers_2 = [1,3,2,4]
+
+# print(numbers_1 == numbers_2)
